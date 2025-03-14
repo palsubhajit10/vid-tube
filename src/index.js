9d37b3a533387170b65e5a67ff0dadf1dbcef4d0
Subhajit Pal <palsubhajit18@gmail.com>
2025-03-14 10:13:07 +0530

    created basic express server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello from index.js!!");
+import dotenv from "dotenv";
+import { app } from "./app.js";
+
+dotenv.config({
+  path: "./config/.env",
+});
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
+});
