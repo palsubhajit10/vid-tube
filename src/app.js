3f29da501ee957dd5783c9d131a0393775a3668d
Subhajit Pal <palsubhajit18@gmail.com>
2025-03-15 23:40:11 +0530

    add health check route ,custom async handler , api response and api response utils

--- src/app.js
+++ src/app.js
@@ -9,8 +9,15 @@ app.use(
   })
 );
 
+//common middleware
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true, limit: "1mb" }));
 app.use(express.static("public"));
+
+//import routes
+import healthCkeckRouter from "./routes/healthcheck.routes.js";
+
+//routes
+app.use("/api/v1/healthcheck", healthCkeckRouter);
 
 export { app };
