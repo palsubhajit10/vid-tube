28314f2fdacfcf77d9dc7e2fe54a9c49d907e71e
Subhajit Pal <palsubhajit18@gmail.com>
2025-03-22 00:13:20 +0530

    added pre hooks and method in userschema

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -48,4 +48,38 @@ const userSchema = new Schema(
   { timestamps: true }
 );
 
+userSchema.pre("save", async function (next) {
+  // Only run this function if password is modified (not on other update functions)
+  if (!this.isModified("password")) return next();
+
+  // Hash password with strength of 12;
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET_KEY,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRE_TIME }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET_KEY,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRE_TIME }
+  );
+};
+
 export const User = mongoose.model("User", userSchema);
